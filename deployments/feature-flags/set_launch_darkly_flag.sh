d5b342eb37b8823866cfbd7b12ff41945f45f284
Carl Caum <carl@carlcaum.com>
2022-02-02 15:56:20 -0800

    Manage flag that was passed in

--- deployments/feature-flags/set_launch_darkly_flag.sh
+++ deployments/feature-flags/set_launch_darkly_flag.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-service=$1
+flag=$1
 environment=$2
 
 if [ $3 == "" ]; then
@@ -11,16 +11,16 @@ fi
 
 #Get current state of the feature flag
 environment_string=".items[0].environments.${environment}.on"
-is_currently_on=`curl -s -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json"  https://app.launchdarkly.com/api/v2/flags/default\?env\=${environment}\&filter\="tags:${service},filterEnv:${environment}" | jq ".items[0].environments.${environment}.on"`
+is_currently_on=`curl -s -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json"  "https://app.launchdarkly.com/api/v2/flags/default/${flag}" | jq ".environments.${environment}.on"`
 
 if [ $is_currently_on == "true" ]; then
   if [ $desired_state == "off" ]; then
-    curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOff\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/auto-dispenser
+    curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOff\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/${flag}
   fi
 fi
 
 if [ $is_currently_on == "false" ]; then
   if [ $desired_state == "on" ]; then
-    curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOn\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/auto-dispenser
+    curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOn\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/${flag}
   fi
 fi
