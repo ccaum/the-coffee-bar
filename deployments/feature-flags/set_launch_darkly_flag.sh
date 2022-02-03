2a9486f07a8af309837d5bf906ad1288cbc9523b
Carl Caum <carl@carlcaum.com>
2022-02-02 16:31:32 -0800

    Quote variables in bash script

--- deployments/feature-flags/set_launch_darkly_flag.sh
+++ deployments/feature-flags/set_launch_darkly_flag.sh
@@ -3,7 +3,7 @@
 flag=$1
 environment=$2
 
-if [ $3 == "" ]; then
+if [ "${3}" == "" ]; then
   desired_state=`get_flag_desired_state.sh`
 else
   desired_state=$3
@@ -13,14 +13,14 @@ fi
 environment_string=".items[0].environments.${environment}.on"
 is_currently_on=`curl -s -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json"  "https://app.launchdarkly.com/api/v2/flags/default/${flag}" | jq ".environments.${environment}.on"`
 
-if [ $is_currently_on == "true" ]; then
-  if [ $desired_state == "off" ]; then
+if [ "${is_currently_on}" == "true" ]; then
+  if [ "${desired_state}" == "off" ]; then
     curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOff\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/${flag}
   fi
 fi
 
-if [ $is_currently_on == "false" ]; then
-  if [ $desired_state == "on" ]; then
+if [ "${is_currently_on}" == "false" ]; then
+  if [ "${desired_state}" == "on" ]; then
     curl -s -X PATCH -H "Authorization: ${LD_API_TOKEN}" -H "Content-Type: application/json; domain-model=launchdarkly.semanticpatch" -d "{ \"environmentKey\": \"${environment}\", \"instructions\": [ {\"kind\": \"turnFlagOn\"} ] }"  https://app.launchdarkly.com/api/v2/flags/default/${flag}
   fi
 fi
